78e7973baa46dce745f848ffafffe1864dce6da5
Crowdbotics <engineering@crowdbotics.com>
2023-08-08 16:24:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cypressapp6f1omcqy-108242",
+  url: "https://cypressapp6f1omcqy-108242.botics.co",
+  api: "https://cypressapp6f1omcqy-108242.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
